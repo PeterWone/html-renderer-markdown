70fb06ac11991d6989b8b95633a527cc9ee9d64e
Peter Wone <peter.wone@outlook.com>
2024-06-27 08:36:26 +1000

    includes scripts

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,12 @@ import * as htmlRendererMarkdown from "./html-renderer-markdown";
 import { Logger } from "winston";
 
 export async function activate(context: vscode.ExtensionContext) {
-  // todo register for your own languageId, passing only the callbacks that you implement
-  // in some cases you may need to define a new language in package.json
-  // todo remove the registration of SVG language from package.json
   const logger = await vscode.commands.executeCommand<Logger>(
     "print.registerDocumentRenderer", "markdown",
     {
       getBodyHtml: htmlRendererMarkdown.getBodyHtml,
       getCssUriStrings: htmlRendererMarkdown.getCssUriStrings,
-      //getScriptUriStrings: htmlRendererMarkdown.getScriptUriStrings,
+      getScriptUriStrings: htmlRendererMarkdown.getScriptUriStrings,
       getResource: htmlRendererMarkdown.getResource
     }
   );
